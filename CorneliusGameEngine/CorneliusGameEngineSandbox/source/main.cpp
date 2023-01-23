925c72f65daf72996b5169136138a46a348615c0
Jack Kellett <71409898+JackKellett02@users.noreply.github.com>
2023-01-23 14:21:39 +0000

    Project structure update
    Project structure update:
     - Projects now output in an extra subfolder under the configuration named by the currently selected platform.
     - Projects now put intermediate files in an extra subfolder under the configuration named by the currently selected platform which includes a subfolder called "intermediate".
    
    I also added some messages upon entry and exit into the sandbox project when running the generated executable.

--- CorneliusGameEngine/CorneliusGameEngineSandbox/source/main.cpp
+++ CorneliusGameEngine/CorneliusGameEngineSandbox/source/main.cpp
@@ -2,8 +2,15 @@
 #include "CorneliusEngine.h"
 
 int main(int argc, char* argv[]) {
-	std::cout << "Test" << std::endl;
+	//Sandbox Entry point.
+	std::cout << "\n--------------------------------------\nLoading Cornelius Engine Sandbox.\n--------------------------------------" << std::endl;
 
+	//TODO: APPLICATION CLASS TO LOAD ENGINE AND RUN PROJECT.
+
+
+
+	//Sandbox Exit Point.
+	std::cout << "\n--------------------------------------\nPress any key to exit the application.\n--------------------------------------" << std::endl;
 	std::cin.get();
 	return 0;
 }
