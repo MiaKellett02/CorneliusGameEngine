ea99483990a116492a2617effb90e4333d8feee3
Mia Kellett <71409898+MiaKellett02@users.noreply.github.com>
2025-01-26 21:18:27 +0000

    Messed with timer a bit more to make it more accurate

--- CorneliusGameEngine/Isometric_Engine_Library/include/Timer.h
+++ CorneliusGameEngine/Isometric_Engine_Library/include/Timer.h
@@ -8,21 +8,28 @@
 #include "Logging.h"
 
 //Declarations.
-struct Timer {
-	std::chrono::time_point<std::chrono::steady_clock> start, end;
-	std::chrono::duration<float, std::milli> duration;
+class Timer {
+public:
 	std::string timerName;
 
 	Timer(std::string a_timerName) {
-		start = std::chrono::high_resolution_clock::now();
+		m_startTimePoint = std::chrono::high_resolution_clock::now();
 		timerName = a_timerName;
 	}
 
 	~Timer() {
-		end = std::chrono::high_resolution_clock::now();
-		duration = end - start;
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+		
+		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_startTimePoint).time_since_epoch().count();
+		auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-		float ms = duration.count();
-		std::cout << timerName << " took " << ms << "ms" << std::endl;
+		auto duration = end - start;
+		double ms = duration * 0.001;
+
+		std::string output = timerName + " took " + std::to_string(ms) + "ms";
+		CorneliusEngine::Log(output);
 	}
+
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_startTimePoint;
 };
